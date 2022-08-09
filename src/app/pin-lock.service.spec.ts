7045e131f81c6ae9fd265cf886ca8a69dd3f4bc7
Pedro <pedro.moreira01@fatec.sp.gov.br>
2022-08-08 22:34:21 -0300

    separating OfflineCrypto.spec.ts, fixed pin-lock.service.spec.ts

--- src/app/pin-lock.service.spec.ts
+++ src/app/pin-lock.service.spec.ts
@@ -3,32 +3,33 @@ import { anyString, Mock, reset, when, same } from 'omnimock';
 
 import { PinLockService, STORAGE_KEY } from './pin-lock.service';
 import { LOCAL_STORAGE } from './platform/providers';
-import { FEATURE_CRYPTO_TYPE } from 'src/misc/FeatureDetector';
+import { CRYPTO_IMPL } from './root-providers';
+import { OfflineService } from './offline.service';
 /*
  * pedro-arruda-moreira: local storage encryption
  */
-/*
-decrypted mock data:
-{
-    "pin": "1234",
-    "data": "53cr37"
-}
-encrypted mock data:*/
-const CT = `{ "iv": "QBGp5xXcIzdSMqykVub9wg==", "v": 1, "iter": 10000, "ks": 128, "ts": 64, "mode": "ccm",
-"adata": "", "cipher": "aes", "salt": "ajhKpL/K8dA=",
-"ct": "D7HQz3xvRAbnnbEXjjWhKl/XAKAcQtc8aTy9YBSglAewu4OTD90="
-}`;
+
 
 
 describe('PinLockServiceTest', () => {
+    const correctPin = '1234';
+    const data = JSON.stringify({pin: parseInt(correctPin), data: "53cr37"});
+    const CT = "this is encrypted.";
+    let isRunningOffline = false;
     let lsMock: Mock<Storage>;
     let service: PinLockService;
-    (window as any).sjcl = require('./../assets/js/sjcl.js');
 
     beforeEach(() => {
+        isRunningOffline = false;
+        when(getMock(OfflineService).isRunningOffline()).call(() => Promise.resolve(isRunningOffline));
+        when(getMock(CRYPTO_IMPL).encrypt(anyString(), correctPin)).return(Promise.resolve(CT)).anyTimes();
+        when(getMock(CRYPTO_IMPL).decrypt(CT, anyString())).call((_, pin) => {
+            if(pin == correctPin) {
+                return Promise.resolve(data);
+            }
+            return Promise.resolve('' + Math.random());
+        }).anyTimes();
         lsMock = getMock(LOCAL_STORAGE);
-        //pedro-arruda-moreira: local storage encryption
-        when(lsMock.getItem(same(FEATURE_CRYPTO_TYPE))).return('offline');
         service = getService(PinLockService);
     });
 	// pedro-arruda-moreira: desktop mode
@@ -38,11 +39,9 @@ describe('PinLockServiceTest', () => {
             /*
 			 * pedro-arruda-moreira: local storage encryption
 			 */
-            let obj = JSON.parse(datum);
-            expect(obj.iter).toEqual(10000);
-            expect(obj.mode).toEqual('ccm');
+            expect(datum).toEqual(CT);
         }).once();
-        await service.setSecret('1234', '53cr37');
+        await service.setSecret(correctPin, '53cr37');
     });
 	// pedro-arruda-moreira: desktop mode
     it('getSecret with good pin returns the secret', async () => {
@@ -54,7 +53,16 @@ describe('PinLockServiceTest', () => {
 			 */
             return CT;
         });
-        expect(await service.getSecret('1234')).toBe('53cr37');
+        expect(await service.getSecret(correctPin)).toBe('53cr37');
+    });
+	// pedro-arruda-moreira: desktop mode
+    it('getSecret with offline mode returns the mock secret', async () => {
+        isRunningOffline = true;
+        expect(JSON.parse(await (service.getSecret(correctPin) as Promise<string>))).toEqual({
+            password: correctPin,
+            url: 'offline://offline',
+            username: 'offline_user'
+        });
     });
 	// pedro-arruda-moreira: desktop mode
     it('getSecret with bad pin returns undefined', async () => {
